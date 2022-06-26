7914197f2efbcf6ecf4b6bc0b520f3886cf721fe
vitoremanuelqf <vitoremanuelqf@gmail.com>
2022-06-25 21:24:50 -0300

    started lesson four

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qc4lu63kql01xx3r005mj8/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)',
+      },
+
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
